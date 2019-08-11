5dffdd9816e13888ef5bac32b4c91a263031c35d
Aibek Ozhorov <ozhorov@gmail.com>
2019-08-11 04:01:48 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,8 +4,8 @@ def square_array(array)
   counter = 0
  while counter < array.length do
    array[counter]**2
-    counter +=1
     basket.push(array[counter]**2)
+    counter +=1
 end
 return basket
 end
